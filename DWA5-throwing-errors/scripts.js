d4b32c4c1bc1aa5580fd989c22418568d8912585
Emil Erasmus <lime.erasmus@gmail.com>
2023-10-04 11:44:44 +0200

    Added "try" and "catch" blocks to handle errors.

---
+++ DWA5-throwing-errors/scripts.js
@@ -0,0 +1,35 @@
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+
+  try {
+    // Scenario: Providing anything that is not a number should crash the program
+    if (isNaN(dividend) || isNaN(divider)) {
+      document.body.innerHTML = "Something critical went wrong. Please reload the page.";
+      throw "Error: Invalid input provided. Please enter valid numbers.";
+    }
+
+    // Scenario: Validation when values are missing
+    if (dividend === "" || divider === "") {
+      result.innerText = "Division not performed. Both values are required in inputs. Try again";
+      return;
+    }
+
+    // Scenario: An invalid division should log an error in the console
+    if (divider <= 0) {
+      result.innerText = "Division not performed. Invalid number provided. Try again";
+      throw "Division not performed. Invalid number provided. Try again";
+    }
+    // This line runs if there are no errors
+    result.innerText = Math.floor(dividend / divider);
+
+  } catch (err) {
+    console.error(err)
+  }
+});
