c701261f6c4552cd5dcb8cf4b6dbf1e67ecbd659
Emil Erasmus <lime.erasmus@gmail.com>
2023-10-05 15:28:52 +0200

    Implemented abstraction.

--- DWA6-abstraction/scripts/scripts.js
+++ DWA6-abstraction/scripts/scripts.js
@@ -3,6 +3,11 @@ import {
   BOOKS_PER_PAGE, authors, genres, books,
 } from './data';
 
+// Import all values from functions.js to be used in this file.
+import {
+  elements, day, night, createBookElements, addSelectOptions, clearList,
+} from './functions';
+
 /*
 totalBooksShown keeps track of the books displayed on the page and is
 incremented by 1 each time a new book is added. */
@@ -10,107 +15,6 @@ let totalBooksShown = 0;
 
 // "fragment" variable houses each book element on the page.
 const fragment = document.createDocumentFragment();
-
-/**
- * This object gets and houses all the html elements used in this file for easy
- * access.
- *
- * @type {object}
- */
-const elements = {
-  header: {
-    search: document.querySelector('[data-header-search]'),
-    settings: document.querySelector('[data-header-settings]'),
-  },
-  main: {
-    items: document.querySelector('[data-list-items]'),
-    message: document.querySelector('[data-list-message]'),
-    button: document.querySelector('[data-list-button]'),
-  },
-  preview: {
-    overlay: document.querySelector('[data-list-active]'),
-    blur: document.querySelector('[data-list-blur]'),
-    image: document.querySelector('[data-list-image]'),
-    title: document.querySelector('[data-list-title]'),
-    subtitle: document.querySelector('[data-list-subtitle]'),
-    description: document.querySelector('[data-list-description]'),
-    close: document.querySelector('[data-list-close]'),
-  },
-  search: {
-    overlay: document.querySelector('[data-search-overlay]'),
-    form: document.querySelector('[data-search-form]'),
-    title: document.querySelector('[data-search-title]'),
-    genre: document.querySelector('[data-search-genres]'),
-    author: document.querySelector('[data-search-authors]'),
-    cancel: document.querySelector('[data-search-cancel]'),
-    search: document.querySelector("button.overlay__button.overlay__button_primary[form='search']"),
-  },
-  settings: {
-    overlay: document.querySelector('[data-settings-overlay]'),
-    form: document.querySelector('[data-settings-form]'),
-    theme: document.querySelector('[data-settings-theme]'),
-    save: document.querySelector("button.overlay__button.overlay__button_primary[form='settings']"),
-    cancel: document.querySelector('[data-settings-cancel]'),
-  },
-};
-
-/*
-An object with colour parameters set for "light mode" to be used in the
-handleAddTheme function */
-const day = {
-  dark: '10, 10, 20',
-  light: '255, 255, 255',
-};
-
-/*
-An object with colour parameters set for "dark mode" to be used in the
-handleAddTheme function */
-const night = {
-  dark: '255, 255, 255',
-  light: '10, 10, 20',
-};
-
-/**
- * A function that takes a book object from the "books" array, destructures it's
- * various values, creates a "div" element, and then inputs each value inside
- * its own element within the newly created "div" element, creating a class for
- * each that corresponds with the class selectors in the css file. It then
- * returns the html element.
- *
- * @param {object} book
- * @returns {HTMLElement}
- */
-const createBookElements = (book) => {
-  // Each "book" object in "books" array is destructured for convenience.
-  const {
-    author, id, image, title, description, published,
-  } = book;
-  // "author" id gets authors name from "authors" object.
-  const authorName = authors[author];
-
-  // Create "div" element to house each book.
-  const bookElement = document.createElement('div');
-  // Set class name to "preview" to correspond with ".preview" selector in css file.
-  bookElement.className = 'preview';
-  // Set unique id for each book element to facilitate specific referencing.
-  bookElement.dataset.id = id;
-
-  /*
-  Set child elements of "bookElement" with each class name corresponding to
-  those in the css file. */
-  bookElement.innerHTML = /* html */ `
-        <img class="preview__image" src="${image}" alt="super-epic-pic"/>
-
-        <div class="preview__info">
-            <p class="preview__title">${title}</p>
-            <div class="preview__author">${authorName}</div>
-            <p class="preview_hidden" id="description">${description}</p>
-            <p class="preview_hidden" id="date">${published}</p>
-        </div>
-    `;
-
-  return bookElement;
-};
 
 /**
  * A handler function that adds 36 books to the 'data-list-items' element on the
@@ -213,47 +117,6 @@ const handleSearchToggle = () => {
     isSearchOpen.open = false;
   } else {
     isSearchOpen.open = true;
-  }
-};
-
-/**
- * A function that adds "option" elements to the "select" elements for genre and
- * authors in the "data-search-overlay" element. It also adds "option" elements
- * that account for all authors and genres.
- *
- * @param {HTMLElement} selectContainer
- * @param {object} dataObj
- * @param {string} allOption
- */
-const addSelectOptions = (selectContainer, dataObj, allOption) => {
-  // Create "option" element for "All Genres/Authors"
-  const allOptionElement = document.createElement('option');
-  allOptionElement.innerText = `All ${allOption}`;
-  selectContainer.appendChild(allOptionElement);
-
-  /*
-  Loop through object passed in (genres or authors), create option element for
-  each, and append to selectContainer */
-  // eslint-disable-next-line
-  for (const [key, val] of Object.entries(dataObj)) {
-    const optionElement = document.createElement('option');
-    optionElement.dataset.id = key;
-
-    optionElement.innerText = val;
-    selectContainer.appendChild(optionElement);
-  }
-};
-
-/**
- * A function that clears the "data-list-items" element of all it's current book
- * elements so that the searched element can be added.
- */
-const clearList = () => {
-  const list = document.querySelector('[data-list-items]');
-  if (list) {
-    while (list.firstChild) {
-      list.removeChild(list.firstChild);
-    }
   }
 };
 
@@ -396,17 +259,14 @@ addSelectOptions(elements.search.author, authors, 'Authors');
 
 // This is the show more button.
 elements.main.button.addEventListener('click', handleAddBooks);
-
 // These buttons open and close the book preview.
 elements.main.items.addEventListener('click', handlePreview);
 elements.preview.close.addEventListener('click', handlePreview);
-
 // These buttons open and close the search overlay.
 elements.header.search.addEventListener('click', handleSearchToggle);
 elements.search.cancel.addEventListener('click', handleSearchToggle);
 // This is the search button.
 elements.search.search.addEventListener('click', handleSearchBooks);
-
 // These buttons open and close the settings overlay.
 elements.header.settings.addEventListener('click', handleThemeSettings);
 elements.settings.cancel.addEventListener('click', handleThemeSettings);
