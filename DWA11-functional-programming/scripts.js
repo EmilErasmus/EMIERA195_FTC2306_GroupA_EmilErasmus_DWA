1b52434dda2f850f8f15f4c060d2c735e88301d3
Emil Erasmus <lime.erasmus@gmail.com>
2023-10-23 16:13:28 +0200

    Improved purity of functions.

--- DWA11-functional-programming/scripts.js
+++ DWA11-functional-programming/scripts.js
@@ -16,10 +16,12 @@ const state = [0];
 
 /**
  * A function that gets the current state of the app.
+ *
+ * @param {array} currentState
  */
-const getState = () => {
+const getState = (currentState) => {
   // eslint-disable-next-line no-console
-  console.log(`current state: ${state[0]}`);
+  console.log(`current state: ${currentState[0]}`);
 };
 
 /**
@@ -27,8 +29,8 @@ const getState = () => {
  *
  * @param {Number} newState;
  */
-const updateState = (newState) => {
-  state.unshift(newState);
+const updateState = (currentState, newState) => {
+  currentState.unshift(newState);
 };
 
 /**
@@ -39,7 +41,7 @@ const handleAdd = () => {
   const newValue = parseInt(elements.num.value, 10) + STEP_AMOUNT;
   elements.num.value = newValue;
 
-  updateState(newValue);
+  updateState(state, newValue);
   getState(state);
 };
 
@@ -51,7 +53,7 @@ const handleSubtract = () => {
   const newValue = parseInt(elements.num.value, 10) - STEP_AMOUNT;
   elements.num.value = newValue;
 
-  updateState(newValue);
+  updateState(state, newValue);
   getState(state);
 };
 
@@ -62,14 +64,14 @@ const handleSubtract = () => {
 const handleReset = () => {
   elements.num.value = 0;
 
-  updateState(0);
+  updateState(state, 0);
   getState(state);
 };
 
 /*
 Get state is run when the page first loads,
 displaying the initial state of the app. */
-getState();
+getState(state);
 
 elements.subtract.addEventListener('click', handleSubtract);
 elements.add.addEventListener('click', handleAdd);
